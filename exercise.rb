ce248447676e142faaedbe3443208f79a4f5e8c1
Daniel Woznicki <tokay89@sbcglobal.net>
2016-04-29 23:35:57 -0700

    Add marklar challenge

--- exercise.rb
+++ exercise.rb
@@ -4,7 +4,25 @@ class Exercise
   # Return a string in which every word in "str" that exceeds 4 characters is replaced with "marklar".
   # If the word being replaced has a capital first letter, it should instead be replaced with "Marklar".
   def self.marklar(str)
-    # TODO: Implement this method
+
+    split = str.split(" ").map do |word|
+      word.split(/([?!.])/)
+    end
+
+    marklared = split.map do |word|
+      if word[0].length > 4
+        if word[0] == word[0].capitalize
+          word[0] = "Marklar"
+        else
+          word[0] = "marklar"
+        end
+      else
+        word[0]
+      end
+      word.join("")
+    end
+
+    return marklared.join(" ")
   end
 
   # Return the sum of all even numbers in the Fibonacci sequence, up to
